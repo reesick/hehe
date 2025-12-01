eacd74a300139515af5c764c4897a74ded3f7ca7
reesick <shreevirajmatale25@gmail.com>
2025-12-01 22:34:04 +0530

    #4

--- 04_greedy_activity_selection.cpp
+++ 04_greedy_activity_selection.cpp
@@ -3,23 +3,23 @@ using namespace std;
 
 int main() {
 
-    
     int start[] = {1, 3, 0, 5, 8, 5};
     int endt[]  = {2, 4, 6, 7, 9, 9};
     int n = 6;
 
+    // First activity select karo
     int last_end = endt[0];
-    cout << start[0] << " " << endt[0] <<endl;
+    cout << start[0] << " " << endt[0] << endl;
 
-    for (int i = 1; i <= n; i++)
+    // Now check the remaining
+    for (int i = 1; i < n; i++)        // FIX #1 → < n
     {
-        if (last_end < start[i])
+        if (start[i] >= last_end)      // FIX #2 → >= not <
         {
-            /* code */
+            cout << start[i] << " " << endt[i] << endl;
+            last_end = endt[i];
         }
-        
     }
-    
 
     return 0;
 }
